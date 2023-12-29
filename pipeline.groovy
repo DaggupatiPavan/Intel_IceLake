009861acf41e9415c5d4d9338950fd0c87fb73ab
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2023-12-29 14:52:04 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,7 +12,7 @@ pipeline {
         stage('Build infra'){
             steps{
                 sh '''
-                echo pavan | sudo -S scp -i /var/lib/jenkins/.ssh/nextgen-devops-team.pem -r ../intel_icelake ubuntu@10.63.20.41:/home/ubuntu
+                echo pavan | sudo -S rsync -e "ssh -i /var/lib/jenkins/.ssh/nextgen-devops-team.pem" -av --exclude=".git" ../intel_icelake ubuntu@10.63.20.41:/home/ubuntu
                 ssh ubuntu@10.63.20.41 -- 'terraform init'
                 ssh ubuntu@10.63.20.41 -- 'terraform validate'
                 ssh ubuntu@10.63.20.41 -- 'terraform plan -out=tfplan'
