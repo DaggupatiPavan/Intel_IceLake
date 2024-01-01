eb68517107fa9c3aa6b841139ad9083b5eba39df
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-01 18:19:44 +0530

    Update PE_Jenkins.groovy

--- PE_Jenkins.groovy
+++ PE_Jenkins.groovy
@@ -45,8 +45,8 @@ pipeline {
                     // sh "sudo ssh-keygen -f /var/lib/jenkins/.ssh/known_hosts -R ${hammer_ip}"
                     // sh "sudo ssh-keyscan ${postgres_ip} >> /var/lib/jenkins/.ssh/known_hosts"
                     // sh "sudo ssh-keyscan ${hammer_ip} >> /var/lib/jenkins/.ssh/known_hosts"
-                    sh "ssh -o StrictHostKeyChecking=yes ubuntu@${postgres_ip} -- 'sudo apt update && sudo apt install ansible -y'"
-                    sh "ssh -o StrictHostKeyChecking=yes ubuntu@${hammer_ip} -- 'sudo apt update && sudo apt install ansible -y'"
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@${postgres_ip} -- 'sudo apt update && sudo apt install ansible -y'"
+                    sh "ssh -o StrictHostKeyChecking=no ubuntu@${hammer_ip} -- 'sudo apt update && sudo apt install ansible -y'"
                 }
             }
         }
