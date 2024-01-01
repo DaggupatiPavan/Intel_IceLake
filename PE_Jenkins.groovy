196362236e734465c1d4912de76c29ae01e6d13a
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-01 11:13:56 +0530

    Update PE_Jenkins.groovy

--- PE_Jenkins.groovy
+++ PE_Jenkins.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
-    parameters{
-        choice(name: 'action',choices: 'apply\ndestroy',description: 'Choose the action you want')
+    parameters {
+        choice(name: 'action', choices: 'apply\ndestroy', description: 'Choose the action you want')
     }
     stages {
         stage('Clone') {
@@ -9,46 +9,45 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/AbhishekRaoV/Intel_IceLake.git'
             }
         }
-        stage('Build infra'){
-            steps{
-                script{
-                  if(params.action == 'destroy'){
-                    sh "terraform destroy --auto-approve "
-                  }
-                  if(params.action == 'apply'){
-                    sh '''
-                    
-                       terraform init
-                       terraform validate
-                       terraform plan -out=tfplan
-                       terraform apply tfplan -no-color
-                       terraform output -json private_ips | jq -r '.[]'
-                    '''
+        stage('Build infra') {
+            steps {
+                script {
+                    if (params.action == 'destroy') {
+                        sh "terraform destroy --auto-approve "
+                    }
+                    if (params.action == 'apply') {
+                        sh '''
+                            terraform init
+                            terraform validate
+                            terraform plan -out=tfplan
+                            terraform apply tfplan -no-color
+                            terraform output -json private_ips | jq -r '.[]'
+                        '''
                     }
                 }
             }
         }
 
-        stage('Generate Inventory file'){
-            steps{
-                script{
-                    sh ./inventoryfile.sh
+        stage('Generate Inventory file') {
+            steps {
+                script {
+                    sh './inventoryfile.sh'
                 }
             }
         }
 
-        stage('Install ansible'){
-            steps{
-                script{
-                    sh" ssh ubuntu@${postgres_ip} -- 'sudo apt install ansible'"
-                    sh" ssh ubuntu@${hammer_ip} -- 'sudo apt install ansible'"  
+        stage('Install ansible') {
+            steps {
+                script {
+                    sh "ssh ubuntu@${postgres_ip} -- 'sudo apt install ansible'"
+                    sh "ssh ubuntu@${hammer_ip} -- 'sudo apt install ansible'"
                 }
             }
         }
 
-        stage('Install Tools'){
-            steps{
-                script{
+        stage('Install Tools') {
+            steps {
+                script {
                     sh """
                         ansible-playbook -i myinventory postgres_install.yaml
                         ansible-playbook -i myinventory hammerdb_install.yaml
@@ -59,9 +58,9 @@ pipeline {
                 }
             }
         }
-        stage('Configure'){
-            steps{
-                script{
+        stage('Configure') {
+            steps {
+                script {
                     sh """
                         ansible-playbook -i myinventory postgres_config.yaml -e postgres_ip=${postgres_ip}, hammer_ip=${hammer_ip}
                         ansible-playbook -i myinventory hammer_config.yaml -e postgres_ip=${postgres_ip}
@@ -74,4 +73,3 @@ pipeline {
         }
     }
 }
-
