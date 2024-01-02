27037eeba69960289dd297ff47a6b393d8407cb3
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-02 13:04:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "ssh_ingress" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = [""0.0.0.0/0""]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
  
 resource "aws_security_group_rule" "custom_port_ingress" {
