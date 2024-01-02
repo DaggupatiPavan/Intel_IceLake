8849caf2dd07d6ff4ac8ce6b86254e2c0fbb1d3e
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-02 16:45:23 +0530

    Update PE_Jenkins.groovy

--- PE_Jenkins.groovy
+++ PE_Jenkins.groovy
@@ -71,10 +71,10 @@ pipeline {
                         ansible-playbook -i myinventory postgres_config.yaml -e postgres_ip=${postgres_ip} -e hammer_ip=${hammer_ip}
                         ansible-playbook -i myinventory hammer_config.yaml -e postgres_ip=${postgres_ip}
                         ansible-playbook -i myinventory postgres_backup.yaml 
-                        
+                        ansible-playbook -i myinventory test_hammer.yaml -e postgres_ip=${postgres_ip}
                         ansible-playbook -i myinventory restore_db.yaml 
                     """
-                    // ansible-playbook -i myinventory test_hammer.yaml -e postgres_ip=${postgres_ip}
+                    
                 }
             }
         }
