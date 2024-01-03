6abf31a45ee8a88118ed7a3c798238a60e0eff0d
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-03 13:16:16 +0530

    Update PE_Jenkins.groovy

--- PE_Jenkins.groovy
+++ PE_Jenkins.groovy
@@ -57,6 +57,7 @@ pipeline {
                     sh """
                         ansible-playbook -i myinventory postgres_install.yaml
                         ansible-playbook -i myinventory hammerdb_install.yaml
+                        ansible-playbook -i myinventory node_exporter_install.yaml
                         ansible-playbook -i myinventory prometheus_install.yaml
                         ansible-playbook -i myinventory postgres_exporter_install.yaml -e postgres_ip=${postgres_ip}
                         ansible-playbook -i myinventory grafana_install.yaml
