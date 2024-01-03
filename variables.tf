a9197158c405250c94486ebc2420b92746b8e47c
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-03 17:56:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "ami" {}
+variable "volume_type" {}
+variable "instance_count" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "security_group_id" {}
+variable "key_name" {}
+variable "volume_size" {}
