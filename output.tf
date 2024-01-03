e90713bd192efd521a1512cb1d0d19fca2422d1b
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-01-03 17:53:37 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
-output "instance_private_ip" {
-   value = aws_instance.example_instance[*].private_ip
-}
-output "instance_IDs" {
-  value = aws_instance.example_instance[*].id
+output "private_ips" {
+  value=module.ec2-vm.*.private_ip
 }
