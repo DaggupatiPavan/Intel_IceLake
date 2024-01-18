6a2b02ed4198d95bd641e768205f3c04272d039a
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2024-01-18 10:38:32 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,6 +1,7 @@
 module "azure-vm" {
   source                              = "intel/azure-linux-vm/intel"
   azurerm_resource_group_name         = var.rg
+admin_password="Test@12345"
   azurerm_virtual_network_name        = var.vnet
   azurerm_subnet_name                 = var.subnet_id
   virtual_network_resource_group_name = var.rg
@@ -10,6 +11,6 @@ module "azure-vm" {
   source_image_reference_publisher    = var.sirp
   source_image_reference_sku          = var.sirs
   source_image_reference_version      = var.sirv
-  admin_ssh_key                       = var.key
+  admin_ssh_key                       = var.admin_ssh_key
   admin_username                      = var.admin_username
 }
