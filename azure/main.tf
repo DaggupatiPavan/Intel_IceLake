1162f2a143a75af3b3a90d0b39cee4b5b03cca07
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2024-01-18 10:02:50 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -3,6 +3,7 @@ module "azure-vm" {
   azurerm_resource_group_name         = var.rg
   admin_password                      = "Test@123"
   azurerm_virtual_network_name        = var.vnet
+  admin_username                      = var.admin_username
   azurerm_subnet_name                 = var.subnet_id
   virtual_network_resource_group_name = var.rg
   os_disk_storage_account_type        = var.volume_type
